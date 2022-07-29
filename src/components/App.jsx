8b15df995e0df9f93233a030a4b277715b2f2e2f
abdullah <abdullahatik483@gmail.com>
2022-07-29 20:09:16 +0600

    commited

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 
-import Profile from "./profile";
+// import Profile from "./profile";
 // import Skills from "./profile/Skills";
 
-import "./profile/profile.style.css";
+// import "./profile/profile.style.css";
 
 export default class App extends React.Component {
+  getContext(context) {
+    console.log(context)
+  }
+
   render() {
+    this.getContext(this)
     return (
       <div>
-        {/* <h1 className="hello">Hello React, You Are Awesome</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ex, libero beatae, illum animi hic omnis expedita iste recusandae aspernatur earum dicta voluptate! Autem aspernatur maiores, rem animi tempore ea?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, esse. Hic natus reiciendis repellendus rerum nisi deleniti tempora laborum eaque ut repudiandae soluta velit dicta, recusandae, reprehenderit sunt? Perspiciatis, quasi!</p> */}
+        <h1 className="hello">Hello React, You Are Awesome</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ex, libero beatae, illum animi hic omnis expedita iste recusandae aspernatur earum dicta voluptate! Autem aspernatur maiores, rem animi tempore ea?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, esse. Hic natus reiciendis repellendus rerum nisi deleniti tempora laborum eaque ut repudiandae soluta velit dicta, recusandae, reprehenderit sunt? Perspiciatis, quasi!</p>
 
-        <Profile />
+        {/* <Profile /> */}
         
         {/* <div style={{marginTop: '30px', marginBottom: '30px'}}>
           <h3>Lists of Programmers</h3>
