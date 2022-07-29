49fa569f26a6a7e2140295601ef4bdf5a0070e72
abdullah <abdullahatik483@gmail.com>
2022-07-29 12:49:39 +0600

    commit 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
+import Profile from "./profile";
+
 export default class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello React, You Are Awesome</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {/* <h1 className="hello">Hello React, You Are Awesome</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ex, libero beatae, illum animi hic omnis expedita iste recusandae aspernatur earum dicta voluptate! Autem aspernatur maiores, rem animi tempore ea?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, esse. Hic natus reiciendis repellendus rerum nisi deleniti tempora laborum eaque ut repudiandae soluta velit dicta, recusandae, reprehenderit sunt? Perspiciatis, quasi!</p> */}
+
+        <Profile />
+      </div>
+    );
+  }
 }
+
+// function myFunctionComponent() {
+//   const name = "Abdullah Mazumder";
+//   return <h1>i am functional </h1>;
+// }
