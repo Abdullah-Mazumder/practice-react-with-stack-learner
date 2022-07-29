f7f03c607b3c9fe55b644bcee6d24910c09f18a3
abdullah <abdullahatik483@gmail.com>
2022-07-29 21:17:27 +0600

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,20 +5,127 @@ import React from "react";
 
 // import "./profile/profile.style.css";
 
+// class Child extends React.Component {
+//   render() {
+//     this.props.func(this)
+//     return <h1>I am child</h1>
+//   }
+// }
+
+// const ChildComponents = (props) => {
+//   return (
+//     <div>
+//       <h3>I am child component</h3>
+//       <p>I dont know what to do?</p>
+//       {props.children}
+//     </div>
+//   )
+// }
+
 export default class App extends React.Component {
-  getContext(context) {
-    console.log(context)
+  // constructor(props) {
+  //   super(props);
+  //   this.count = 0;
+  // }
+
+  // state = {
+  //   count: 0,
+  // };
+
+  state = {
+    count: 0,
+  };
+
+  increment = async () => {
+    await this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  decrement = async () => {
+    if (this.state.count > 0) {
+      await this.setState({
+        count: this.state.count - 1
+      })
+    }
+  }
+
+  // getContext(context) {
+  //   console.log(context)
+  // }
+
+  // changeCount = async () => {
+  //   await this.setState((prev) => {
+  //     return {
+  //       count: prev.count + 1,
+  //     };
+  //   });
+
+  //   console.log(this.state.count);
+  // };
+
+  intervalId = null;
+
+  startTimer = () => {
+    if(this.state.count > 0 && !this.intervalId) {
+      this.intervalId = setInterval(() => {
+        this.setState({
+          count: this.state.count - 1
+        }, () => {
+          if (this.state.count === 0) {
+            alert('timar finished');
+            clearInterval(this.intervalId)
+            this.intervalId = null;
+          }
+        })
+      }, 1000)
+    }
+  }
+
+  stopTimer = () => {
+    if (this.intervalId) {
+      clearInterval(this.intervalId)
+      this.intervalId = null;
+    }
+  }
+
+  resetTimer = () => {
+    this.setState({count: 0});
+    clearInterval(this.intervalId)
+    this.intervalId = null;
   }
 
   render() {
-    this.getContext(this)
+    // this.getContext(this)
     return (
       <div>
         <h1 className="hello">Hello React, You Are Awesome</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ex, libero beatae, illum animi hic omnis expedita iste recusandae aspernatur earum dicta voluptate! Autem aspernatur maiores, rem animi tempore ea?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, esse. Hic natus reiciendis repellendus rerum nisi deleniti tempora laborum eaque ut repudiandae soluta velit dicta, recusandae, reprehenderit sunt? Perspiciatis, quasi!</p>
+
+
+        <div className="container">
+          <button onClick={this.decrement}> - </button>
+          <span>{this.state.count}</span>
+          <button onClick={this.increment}> + </button>
+          <div className="container">
+            <button onClick={this.startTimer}>start</button>
+            <button onClick={this.stopTimer}>stop</button>
+            <button onClick={this.resetTimer}>reset</button>
+          </div>
+        </div>
+
+
+        {/* <h1>Count - {this.state.count}</h1>
+        <button onClick={this.changeCount}>Add +1</button> */}
+
+        {/* <Child func={this.getContext} /> */}
+
+        {/* <ChildComponents>
+          <h2>hello i am from parent</h2>
+          <h4>i am child of Child component</h4>
+        </ChildComponents> */}
 
         {/* <Profile /> */}
-        
+
         {/* <div style={{marginTop: '30px', marginBottom: '30px'}}>
           <h3>Lists of Programmers</h3>
           <p>Mr. X</p>
