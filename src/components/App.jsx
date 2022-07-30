82026d3d79f3f52236889ba4c5e943b147d2627e
abdullah <abdullahatik483@gmail.com>
2022-07-30 09:46:14 +0600

    form elements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -32,23 +32,23 @@ export default class App extends React.Component {
   //   count: 0,
   // };
 
-  state = {
-    count: 0,
-  };
+  // state = {
+  //   count: 0,
+  // };
 
-  increment = async () => {
-    await this.setState({
-      count: this.state.count + 1
-    })
-  }
+  // increment = async () => {
+  //   await this.setState({
+  //     count: this.state.count + 1
+  //   })
+  // }
 
-  decrement = async () => {
-    if (this.state.count > 0) {
-      await this.setState({
-        count: this.state.count - 1
-      })
-    }
-  }
+  // decrement = async () => {
+  //   if (this.state.count > 0) {
+  //     await this.setState({
+  //       count: this.state.count - 1
+  //     })
+  //   }
+  // }
 
   // getContext(context) {
   //   console.log(context)
@@ -64,45 +64,230 @@ export default class App extends React.Component {
   //   console.log(this.state.count);
   // };
 
-  intervalId = null;
+  // intervalId = null;
 
-  startTimer = () => {
-    if(this.state.count > 0 && !this.intervalId) {
-      this.intervalId = setInterval(() => {
-        this.setState({
-          count: this.state.count - 1
-        }, () => {
-          if (this.state.count === 0) {
-            alert('timar finished');
-            clearInterval(this.intervalId)
-            this.intervalId = null;
-          }
-        })
-      }, 1000)
+  // startTimer = () => {
+  //   if(this.state.count > 0 && !this.intervalId) {
+  //     this.intervalId = setInterval(() => {
+  //       this.setState({
+  //         count: this.state.count - 1
+  //       }, () => {
+  //         if (this.state.count === 0) {
+  //           alert('timar finished');
+  //           clearInterval(this.intervalId)
+  //           this.intervalId = null;
+  //         }
+  //       })
+  //     }, 1000)
+  //   }
+  // }
+
+  // stopTimer = () => {
+  //   if (this.intervalId) {
+  //     clearInterval(this.intervalId)
+  //     this.intervalId = null;
+  //   }
+  // }
+
+  // resetTimer = () => {
+  //   this.setState({count: 0});
+  //   clearInterval(this.intervalId)
+  //   this.intervalId = null;
+  // }
+
+  // handleButtonClick = (event) => {
+  //   console.log(event.target);
+  // };
+
+  // state = {
+  //   name: "",
+  // };
+
+  // handleChange = (e) => {
+  //   this.setState({ name: e.target.value });
+  // };
+
+  // handleFocus = (e) => {
+  //   console.log("I am focus event");
+  // };
+
+  // handleBlur = (e) => {
+  //   if (!this.state.name) {
+  //     alert("please enter your name");
+  //   }
+  // };
+
+  state = {
+    name: "",
+    country: "bangladesh",
+    bio: "",
+    birthDay: "",
+    gender: "",
+    agree: false,
+    skills: [],
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleCheckbox = (e) => {
+    this.setState({
+      agree: e.target.checked,
+    });
+  };
+
+  handleSkillChange = (e) => {
+    if (e.target.checked) {
+      this.setState({
+        skills: [...this.state.skills, e.target.value]
+      })
+    } else {
+      const skills = this.state.skills.filter((skill) => skill  !== e.target.value)
+      this.setState({
+        skills
+      })
     }
-  }
-
-  stopTimer = () => {
-    if (this.intervalId) {
-      clearInterval(this.intervalId)
-      this.intervalId = null;
-    }
-  }
-
-  resetTimer = () => {
-    this.setState({count: 0});
-    clearInterval(this.intervalId)
-    this.intervalId = null;
   }
 
   render() {
     // this.getContext(this)
+    const { name, country, bio, birthDay, agree, skills } = this.state;
     return (
-      <div>
-        <h1 className="hello">Hello React, You Are Awesome</h1>
+      <div className="container card card-body mt-5">
+        <h1 className="hello text-center">Hello React, You Are Awesome</h1>
 
+        <div className="card card-body">
+          <input
+            className="form-control"
+            type="text"
+            name="name"
+            placeholder="name"
+            value={name}
+            onChange={this.handleChange}
+          />
+          <br />
+          <select
+            name="country"
+            className="form-control"
+            onChange={this.handleChange}
+            value={country}
+          >
+            <option value="bangladesh">Bangladesh</option>
+            <option value="pakistan">Pakistan</option>
+            <option value="india">India</option>
+          </select>
+          <br />
+          <textarea
+            name="bio"
+            placeholder="bio"
+            className="form-control"
+            value={bio}
+            onChange={this.handleChange}
+          ></textarea>
+          <br />
+          <input
+            type="date"
+            name="birthDay"
+            className="form-control"
+            onChange={this.handleChange}
+            value={birthDay}
+          />
+          <br />
+          <div>
+            <input
+              type="radio"
+              name="gender"
+              value="male"
+              onChange={this.handleChange}
+            />
+            Male <br />
+            <input
+              type="radio"
+              name="gender"
+              value="female"
+              onChange={this.handleChange}
+            />
+            Female <br />
+            <input
+              type="radio"
+              name="gender"
+              value="other"
+              onChange={this.handleChange}
+            />
+            Other <br />
+          </div>
+          <div>
+            <h4>Skills: </h4>
+            <input
+              type="checkbox"
+              name="skills"
+              value="java"
+              checked={skills.includes("java")}
+              onChange={this.handleSkillChange}
+            />Java
+            <br />
+            <input
+              type="checkbox"
+              name="skills"
+              value="javascript"
+              checked={skills.includes("javascript")}
+              onChange={this.handleSkillChange}
+            />javascript
+            <br />
+            <input
+              type="checkbox"
+              name="skills"
+              value="python"
+              checked={skills.includes("python")}
+              onChange={this.handleSkillChange}
+            />python
+            <br />
+            <input
+              type="checkbox"
+              name="skills"
+              value="golang"
+              checked={skills.includes("golang")}
+              onChange={this.handleSkillChange}
+            />golang
+            <br />
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              name="agree"
+              checked={agree}
+              onChange={this.handleCheckbox}
+            />
+            I agrre with all terms and conditions
+            <br />
+            <br />
+          </div>
+          <button
+            className="btn btn-primary"
+            onClick={() => console.log(this.state)}
+          >
+            Show data
+          </button>
+        </div>
 
-        <div className="container">
+        {/* <button onClick={this.handleButtonClick}>Click Me</button>
+        <br />
+        <input
+          type="text"
+          placeholder="Enter some text"
+          value={this.state.name}
+          onChange={this.handleChange}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+        />
+        <br />
+        <br />
+        {this.state.name ? <p>welcome, {this.state.name}</p> : ""} */}
+
+        {/* <div className="container">
           <button onClick={this.decrement}> - </button>
           <span>{this.state.count}</span>
           <button onClick={this.increment}> + </button>
@@ -111,8 +296,7 @@ export default class App extends React.Component {
             <button onClick={this.stopTimer}>stop</button>
             <button onClick={this.resetTimer}>reset</button>
           </div>
-        </div>
-
+        </div> */}
 
         {/* <h1>Count - {this.state.count}</h1>
         <button onClick={this.changeCount}>Add +1</button> */}
